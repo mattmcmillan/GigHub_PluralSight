6a0ed8b525b07933a7836089b678848153987f50
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-04-17 12:11:33 -0700

    Added Users Followed controller and view

---
+++ GigHub/Controllers/UsersFollowedController.cs
@@ -0,0 +1,28 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class UsersFollowedController : Controller
+    {
+        public ApplicationDbContext _context;
+
+        public UsersFollowedController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(a => a.FollowerId == userId)
+                .Select(a => a.UserFollowed)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
